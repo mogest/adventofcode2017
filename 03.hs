f9b1ac370b21558fbdad8f2d86c07d8a72fb03f7
Roger Nesbitt <roger@seriousorange.com>
2017-12-03 23:01:27 +1300

    day 3 code tidy

--- 03.hs
+++ 03.hs
@@ -1,17 +1,20 @@
 import Data.Maybe
 import qualified Data.Map as Map
 
-location :: Int -> (Int, Int)
+type Location = (Int, Int)
+type LocationMap = Map.Map Location Int
+
+location :: Int -> Location
 location number = (x, y)
     where
-        layer = floor $ ((sqrt (fromIntegral number - 1)) + 1) / 2
-        dimension = layer * 2 + 1
-        layerFirst = (dimension - 2) ^ 2 + 1
-        layerPosition = number - layerFirst
-        sideLength = dimension - 1
-        sideMidPoint = sideLength `div` 2 - 1
+        layer                = floor $ ((sqrt (fromIntegral number - 1)) + 1) / 2
+        dimension            = layer * 2 + 1
+        layerFirst           = (dimension - 2) ^ 2 + 1
+        layerPosition        = number - layerFirst
+        sideLength           = dimension - 1
+        sideMidPoint         = sideLength `div` 2 - 1
         (side, sidePosition) = layerPosition `divMod` sideLength
-        sideDistance = sideMidPoint - sidePosition
+        sideDistance         = sideMidPoint - sidePosition
         x = case side of 0 -> layer
                          1 -> sideDistance
                          2 -> -layer
@@ -25,17 +28,19 @@ distance :: Int -> Int
 distance number = abs x + abs y
     where (x, y) = location number
 
-accumulating number grid target =
-    if result > target then
-        result
-    else
-        let newGrid = Map.insert (x, y) result grid
-        in accumulating (number + 1) newGrid target
+accumulating :: Int -> LocationMap -> Int -> Int
+accumulating number grid target
+    | result > target = result
+    | otherwise       = accumulating (number + 1) (Map.insert (x, y) result grid) target
     where
-        (x, y) = location number
-        result = sum $ mapMaybe id [Map.lookup (x + dx, y + dy) grid | dx <- [-1..1], dy <- [-1..1], dx /= 0 || dy /= 0]
+        (x, y)     = location number
+        neighbours = [Map.lookup (x + dx, y + dy) grid | dx <- [-1..1], dy <- [-1..1], dx /= 0 || dy /= 0]
+        result     = sum $ mapMaybe id neighbours
 
-run input = (distance number, accumulating 2 (Map.fromList [((0,0), 1)]) number)
-    where number = read input
+run :: String -> (Int, Int)
+run input = (distance number, accumulating 2 startingLocationMap number)
+    where
+        number = read input
+        startingLocationMap = Map.fromList [((0,0), 1)]
 
 main = interact (show . run)
