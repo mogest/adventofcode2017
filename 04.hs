08311b86e19cbb7a0345264f637d6860c62b6252
Roger Nesbitt <roger@seriousorange.com>
2017-12-04 19:57:32 +1300

    now take out half the code and try again

--- 04.hs
+++ 04.hs
@@ -1,18 +1,10 @@
-import qualified Data.Set as Set
 import Data.List
 
-valid :: (String -> String) -> [String] -> Bool
-valid f words = go Set.empty words
-    where
-        go _   []                 = True
-        go set (unprocessedWord:words)
-            | Set.member word set = False
-            | otherwise           = go (Set.insert word set) words
-            where
-                word = f unprocessedWord
+valid :: [String] -> Bool
+valid words = length words == length (nub words)
 
 validCount :: (String -> String) -> [[String]] -> Int
-validCount f phrases = length $ filter (valid f) phrases
+validCount f phrases = length $ filter (valid . map f) phrases
 
 parse :: String -> [[String]]
 parse = map words . lines
