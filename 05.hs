3439b9e03eb223dfa13d543bdc659da926e8d96f
Roger Nesbitt <roger@seriousorange.com>
2017-12-05 21:59:27 +1300

    fix haskell version so it completes

--- 05.hs
+++ 05.hs
@@ -1,22 +1,19 @@
 import qualified Data.Sequence as S
 
-updaterA :: Int -> Int
-updaterA offset = offset + 1
-
 updaterB :: Int -> Int
 updaterB offset
   | offset >= 3 = offset - 1
   | otherwise   = offset + 1
 
 execute :: (Int -> Int) -> [Int] -> Int
-execute updater offsets = go updater (S.fromList offsets) 0 0
+execute updater offsets = go (S.fromList offsets) 0 0
   where
-    go updater offsets pointer counter =
+    go offsets pointer counter =
       case S.lookup pointer offsets of
         Nothing     -> counter
-        Just offset -> go updater (S.update pointer (updater offset) offsets) (pointer + offset) (counter + 1)
+        Just offset -> go (S.update pointer (updater offset) offsets) (pointer + offset) $! (counter + 1)
 
 run :: [Int] -> (Int, Int)
-run offsets = (execute updaterA offsets, execute updaterB offsets)
+run offsets = (execute (1 +) offsets, execute updaterB offsets)
 
 main = interact (show . run . map read . lines)
