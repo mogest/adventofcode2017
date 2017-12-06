95fde27ce622ac6d9de65242af7b975214c6f3d0
Roger Nesbitt <roger@seriousorange.com>
2017-12-06 21:58:26 +1300

    refactor using Data.Vector

--- 06.hs
+++ 06.hs
@@ -1,29 +1,27 @@
 import qualified Data.Map as Map
-import qualified Data.Sequence as Seq
+import qualified Data.Vector.Unboxed as V
 
-type Banks = Seq.Seq Int
+type Banks = V.Vector Int
 
-recompute :: Banks -> Banks
-recompute banks =
-  go (nextIndex index) blocks (Seq.update index 0 banks)
-  where
-    blocks                = maximum banks
-    Just index            = Seq.elemIndexL blocks banks
-    go index 0     banks  = banks
-    go index count banks  = go (nextIndex index) (count - 1) (increment index banks)
-    increment index banks = Seq.update index (banks `Seq.index` index + 1) banks
-    nextIndex index       = if Seq.length banks == index + 1 then 0 else index + 1
+balance :: Banks -> Banks
+balance banks =
+  V.accum (+) banks updates
+    where
+      blocks     = V.maximum banks
+      Just index = V.elemIndex blocks banks
+      indexes    = take blocks $ drop (index + 1) $ cycle [0 .. V.length banks - 1]
+      updates    = (index, -blocks) : zip indexes (repeat 1)
 
-compute :: Banks -> (Int, Int)
-compute banks =
+reallocate :: Banks -> (Int, Int)
+reallocate banks =
   go 0 banks Map.empty
-  where
-    go count banks seen =
-      case Map.lookup banks seen of
-        Just loopCount -> (count, count - loopCount)
-        Nothing        -> go (count + 1) (recompute banks) (Map.insert banks count seen)
+    where
+      go count banks seen =
+        case Map.lookup banks seen of
+          Just loopCount -> (count, count - loopCount)
+          Nothing        -> go (count + 1) (balance banks) (Map.insert banks count seen)
 
 run :: [Int] -> (Int, Int)
-run banks = compute $ Seq.fromList banks
+run banks = reallocate $ V.fromList banks
 
 main = interact (show . run . map read . words)
