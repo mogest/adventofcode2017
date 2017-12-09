0416b565106a6806972b09470388dda205e23b79
Roger Nesbitt <roger@seriousorange.com>
2017-12-09 15:02:16 +1300

    day 8

---
+++ 08.hs
@@ -0,0 +1,55 @@
+import Data.Maybe
+import qualified Data.Map as Map
+
+data Command = Command { register :: String
+                       , instruction :: String
+                       , value :: Int
+                       , testRegister :: String
+                       , testOperator :: String
+                       , testValue :: Int
+                       } deriving Show
+
+type Registers = Map.Map String Int
+
+load :: String -> Registers -> Int
+load register registers = fromMaybe 0 $ registers Map.!? register
+
+update :: String -> (Int -> Int) -> Registers -> Registers
+update register updater registers = Map.insert register (updater $ load register registers) registers
+
+evaluateIf :: Registers -> Command -> Bool
+evaluateIf registers command
+  | op == ">"  = v1 >  v2
+  | op == "<"  = v1 <  v2
+  | op == ">=" = v1 >= v2
+  | op == "<=" = v1 <= v2
+  | op == "==" = v1 == v2
+  | op == "!=" = v1 /= v2
+    where
+      v1 = load (testRegister command) registers
+      v2 = testValue command
+      op = testOperator command
+
+execute :: Registers -> Command -> Registers
+execute registers command
+  | evaluateIf registers command = execute'
+  | otherwise                    = registers
+    where
+      ins = instruction command
+      reg = register command
+      val = value command
+      execute'
+        | ins == "inc" = update reg (val +) registers
+        | ins == "dec" = update reg (flip (-) val) registers
+
+run :: [Command] -> (Int, Int)
+run commands = (largestAtEnd, largestAtAnyTime)
+  where
+    largestAtAnyTime = maximum $ concat $ map Map.elems history
+    largestAtEnd = maximum $ Map.elems $ last history
+    history = scanl execute Map.empty commands
+
+parse :: [String] -> Command
+parse (a:b:c:"if":d:e:f:[]) = Command a b (read c) d e (read f)
+
+main = interact (show . run . map (parse . words) . lines)
